b499fe4a6fb36a77aa5bf45cfc04f18503e9f78a
magilla <andrew.magill@austintexas.gov>
2015-01-16 20:56:17 -0600

    factory

--- app/services/MyFactory.js
+++ app/services/MyFactory.js
@@ -1,14 +1,17 @@
 define([
   'app'
 ], function (app) {
-  app.factory('notify', ['$window', function(win) {
+  app.factory('notify', ['$window', '$log', function($win, $log) {
+    /////
    var msgs = [];
    return function(msg) {
      msgs.push(msg);
      if (msgs.length == 3) {
-       win.alert(msgs.join("\n"));
+       $win.alert(msgs.join("\n"));
+       $log.log(msgs);
        msgs = [];
      }
    };
-  }]);
+    /////
+  }]);  
 });
